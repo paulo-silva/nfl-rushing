5ff35be48d7f7057f1c0fde4d00c16240a42ed27
Paulo da Silva <paulo_silvalearn@hotmail.com>
2021-01-09 22:03:29 -0300

    [BACKEND] Add seeds.

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -1,11 +1,72 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Server.Repo.insert!(%Server.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Server.{Players}
+
+filename = "./priv/repo/samples/rushing.json"
+
+rushing_samples =
+  filename
+  |> File.read!()
+  |> Jason.decode!()
+
+# Sample validation
+attrs = [
+  "Player",
+  "Team",
+  "Pos",
+  "Att/G",
+  "Att",
+  "Yds",
+  "Avg",
+  "Yds/G",
+  "TD",
+  "Lng",
+  "1st",
+  "1st%",
+  "20+",
+  "40+",
+  "FUM"
+]
+
+case Enum.all?(rushing_samples, &(Map.keys(&1) -- attrs == [])) do
+  true ->
+    "noop"
+
+  false ->
+    raise ArgumentError, message: "#{filename} has some invalid data"
+end
+
+# Persisting Players
+
+defmodule SeedHelpers do
+  def convert_to_string(val) when is_binary(val), do: val
+  def convert_to_string(val), do: to_string(val)
+
+  def convert_to_int(val) when is_integer(val), do: val
+  def convert_to_int(val), do: String.replace(val, [".", ","], "") |> String.to_integer()
+
+  def convert_to_float(val) do
+    case Float.parse(to_string(val)) do
+      :error -> nil
+      {float_val, _} -> float_val
+    end
+  end
+end
+
+Enum.each(rushing_samples, fn player_data ->
+  Players.create_player(%{
+    name: player_data["Player"],
+    team: player_data["Team"],
+    position: player_data["Pos"],
+    attempts_avg: player_data["Att/G"] |> SeedHelpers.convert_to_float(),
+    attempts: player_data["Att"] |> SeedHelpers.convert_to_int(),
+    total_yards: player_data["Yds"] |> SeedHelpers.convert_to_int(),
+    avg_yards_per_attempts: player_data["Avg"] |> SeedHelpers.convert_to_float(),
+    yards_per_game: player_data["Yds/G"] |> SeedHelpers.convert_to_float(),
+    total_touchdowns: player_data["TD"] |> SeedHelpers.convert_to_int(),
+    longest_rush: player_data["Lng"] |> SeedHelpers.convert_to_string(),
+    first_down: player_data["1st"] |> SeedHelpers.convert_to_int(),
+    first_down_ratio: player_data["1st%"] |> SeedHelpers.convert_to_float(),
+    twenty_yards: player_data["20+"] |> SeedHelpers.convert_to_int(),
+    fourty_yards: player_data["40+"] |> SeedHelpers.convert_to_int(),
+    fumbles: player_data["FUM"] |> SeedHelpers.convert_to_int()
+  })
+end)
